cc4a8b6688fb798fa55a6f492f654c135c6e95d9
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-17 10:02:15 -0800

    Initial add
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ TestBuildAndSign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestBuildAndSign
+//
+//  Created by Jason Leach on 2019-03-27.
+//  Copyright © 2019 Jason Leach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
