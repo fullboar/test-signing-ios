0fa41a1f3505cf50ebd3e07cc8b5e259048c325b
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-17 11:08:47 -0800

    Work
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makekc.sh
+++ scripts/makekc.sh
@@ -4,35 +4,25 @@ KC_NAME=cicd.keychain
 CERT_PATH=certificates.p12
 PROFILE_ID=82d0bc53-1708-4833-884f-e3348c30fdbb
 
-echo "Keychain Starting"
+echo ">> Build Keychain Starting... 🤞"
 
-# # check if target keychain exists:
-# /usr/bin/security list-keychains -d user | grep $KC_NAME &> /dev/null
-# rv=$?
 
-# if [[ $rv == 0 ]]; then
-#   # unlock keychain as it's always locked after reboot:
-#   echo "Unlocking keychain"
-#   /usr/bin/security unlock-keychain -p $1 $KC_NAME
-#   /usr/bin/security show-keychain-info $KC_NAME
-# else
-
-echo "Extracting Artifats"
+echo ">> Extracting Artifats"
 
 mkdir -p "~/MobileDevice/Provisioning\ Profiles"
-
 echo ${CERTIFICATE} | base64 -d > $CERT_PATH
 echo ${PROVISIONING_PROFILE} | base64 -d > "~/MobileDevice/Provisioning\ Profiles/${PROFILE_ID}.mobileprovision"
 
-echo "Building keychian"
+echo ">> Create Keychain $KC_NAME"
 /usr/bin/security create-keychain -p $1 $KC_NAME
 /usr/bin/security unlock-keychain -p $1 $KC_NAME
 /usr/bin/security list-keychains -d user -s $KC_NAME
+
+echo ">> Importing Certificate"
 /usr/bin/security import $CERT_PATH -k $KC_NAME -P $1 -T /usr/bin/codesign
 /usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME
 /usr/bin/security set-keychain-settings $KC_NAME
-# fi
 
-echo "Keychain Finished"
+echo ">> Build Keychain Finished. 🤗"
 
 exit 0
