e2501c2d8906b17891876e551823c2a129fd68ab
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-17 12:48:39 -0800

    Work
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ scripts/makepp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ">> Build Provisioning Profile... 🤞"
+
+mkdir -p ~/MobileDevice/Provisioning\ Profiles
+
+echo ${PROVISIONING_PROFILE} | base64 -d > "~/MobileDevice/Provisioning\ Profiles/${PROFILE_ID}.mobileprovision"
+
+ls -lah ~/MobileDevice/Provisioning\ Profiles
+
+echo ">> Build Keychain Finished. 🤗"
+
+exit 0
