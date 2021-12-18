37ff059630a7d8e709bce2b134467125724f8c3f
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-17 17:28:32 -0800

    Work
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makekc.sh
+++ scripts/makekc.sh
@@ -5,10 +5,6 @@ CERT_PATH=certificates.p12
 
 echo ">> Build Keychain Starting... 🤞"
 
-echo ${APPLEWWDRCAG3_CERT} | base64 -d > AppleWWDRCAG3.cer.der
-ls -lah AppleWWDRCAG3.cer.der
-/usr/bin/security import AppleWWDRCAG3.cer.der -k $KC_NAME -P $1
-
 echo ">> Extracting Artifats"
 echo ${CERTIFICATE} | base64 -d > $CERT_PATH
 
@@ -22,6 +18,11 @@ echo ">> Importing Certificate"
 /usr/bin/security import $CERT_PATH -k $KC_NAME -P $1 -T /usr/bin/codesign
 /usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME
 /usr/bin/security set-keychain-settings $KC_NAME
+
+echo ">> Importing AppleWWDRCAG3"
+echo ${APPLEWWDRCAG3_CERT} | base64 -d > AppleWWDRCAG3.cer.der
+/usr/bin/security import AppleWWDRCAG3.cer.der -k $KC_NAME -P $1
+
 
 echo ">> Build Keychain Finished. 🤗"
 
