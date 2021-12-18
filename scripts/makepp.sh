f9e020fbf070a283eee7320774d537ca86e26232
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-17 17:37:41 -0800

    Work
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makepp.sh
+++ scripts/makepp.sh
@@ -6,14 +6,14 @@ PP_DIR="${HOME}/Library/MobileDevice/Provisioning\ Profiles"
 echo ">> Build Provisioning Profile... 🤞"
 echo ">> PP Home = ${PP_DIR}"
 
-ls -lah ${PP_DIR}
+ls -lah "${PP_DIR}"
 ls -lah ~/
 
-mkdir -p ${PP_DIR}
+mkdir -p "${PP_DIR}"
 
-echo ${PROVISIONING_PROFILE} | base64 -d > ${PP_DIR}/${PROFILE_ID}.mobileprovision
+echo ${PROVISIONING_PROFILE} | base64 -d > "${PP_DIR}/${PROFILE_ID}.mobileprovision"
 
-ls -lah ${PP_DIR}
+ls -lah "${PP_DIR}"
 
 echo ">> Build Keychain Finished. 🤗"
 
