f2609294ad01a005ea7b3cf158e3e9a317e88c37
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-20 10:02:51 -0800

    Work
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makekc.sh
+++ scripts/makekc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# KC_NAME=cicd.keychain
-KC_NAME=login.keychain
+KC_NAME=cicd.keychain
+# KC_NAME=login.keychain
 CERT_PATH=certificates.p12
 
 echo ">> Build Keychain Starting... 🤞"
@@ -10,19 +10,19 @@ echo ">> Extracting Artifats"
 echo ${CERTIFICATE} | base64 -d > $CERT_PATH
 
 echo ">> Create Keychain $KC_NAME"
-/usr/bin/security create-keychain -p "" $KC_NAME
+/usr/bin/security create-keychain -p $1 $KC_NAME
 /usr/bin/security default-keychain -s $KC_NAME
-/usr/bin/security unlock-keychain -p "" $KC_NAME
+/usr/bin/security unlock-keychain -p $1 $KC_NAME
 /usr/bin/security list-keychains -d user -s $KC_NAME
 
 echo ">> Importing Certificate"
-/usr/bin/security import $CERT_PATH -k $KC_NAME -P "" -T /usr/bin/codesign
-/usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "" $KC_NAME
+/usr/bin/security import $CERT_PATH -k $KC_NAME -P $1 -T /usr/bin/codesign
+/usr/bin/security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $1 $KC_NAME
 /usr/bin/security set-keychain-settings $KC_NAME
 
 echo ">> Importing AppleWWDRCAG3"
 echo ${APPLEWWDRCAG3_CERT} | base64 -d > AppleWWDRCAG3.cer.der
-/usr/bin/security import AppleWWDRCAG3.cer.der -k $KC_NAME -P ""
+/usr/bin/security import AppleWWDRCAG3.cer.der -k $KC_NAME -P $1
 
 
 echo ">> Build Keychain Finished. 🤗"
