89162689f509cc9167cad49887576858ddcd6206
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-21 12:26:16 -0800

    3
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makepp.sh
+++ scripts/makepp.sh
@@ -10,8 +10,6 @@ mkdir -p "${PP_DIR}"
 
 uuid=$(/usr/libexec/plistbuddy -c Print:UUID /dev/stdin <<< `echo "${PROVISIONING_PROFILE}" | base64 -d | security cms -D`)
 echo "${PROVISIONING_PROFILE}" | base64 -d >"${PP_DIR}/${uuid}.mobileprovision"
-
-ls -lah "${PP_DIR}"
 md5 "${PP_DIR}/${PROFILE_ID}.mobileprovision"
 
 echo ">> Build Keychain Finished. 🤗"
