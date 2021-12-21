c256a6a6009761de9aad986f103e95c1d480830a
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-21 12:30:01 -0800

    3
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makepp.sh
+++ scripts/makepp.sh
@@ -8,9 +8,9 @@ echo ">> Provisioning Profile Home = ${PP_DIR}"
 
 mkdir -p "${PP_DIR}"
 
-uuid=$(/usr/libexec/plistbuddy -c Print:UUID /dev/stdin <<< `echo "${PROVISIONING_PROFILE}" | base64 -d | security cms -D`)
-echo "${PROVISIONING_PROFILE}" | base64 -d >"${PP_DIR}/${uuid}.mobileprovision"
-md5 "${PP_DIR}/${uuid}.mobileprovision"
+UUID=$(/usr/libexec/plistbuddy -c Print:UUID /dev/stdin <<< `echo "${PROVISIONING_PROFILE}" | base64 -d | security cms -D`)
+echo "${PROVISIONING_PROFILE}" | base64 -d >"${PP_DIR}/${UUID}.mobileprovision"
+md5 "${PP_DIR}/${UUID}.mobileprovision"
 
 echo ">> Build Provisioning Profile. 🤗"
 
