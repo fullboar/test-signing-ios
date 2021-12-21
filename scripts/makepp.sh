c0f703be06c02d238449892d098ef1950ab0d352
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-21 12:15:28 -0800

    3
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makepp.sh
+++ scripts/makepp.sh
@@ -8,7 +8,7 @@ echo ">> Provisioning Profile Home = ${PP_DIR}"
 
 mkdir -p "${PP_DIR}"
 
-echo -n "${PROVISIONING_PROFILE}" | base64 --decode --output "${PP_DIR}/${PROFILE_ID}.mobileprovision"
+echo "${PROVISIONING_PROFILE}" | base64 --d >"${PP_DIR}/${PROFILE_ID}.mobileprovision"
 
 ls -lah "${PP_DIR}"
 md5 "${PP_DIR}/${PROFILE_ID}.mobileprovision"
