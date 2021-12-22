8ba5abfdb47a2c6daf16de446cc50540b1447fa6
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-22 10:04:22 -0800

    Work
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- scripts/makepp.sh
+++ scripts/makepp.sh
@@ -7,25 +7,10 @@ echo ">> Build Provisioning Profile... 🤞"
 echo ">> Provisioning Profile Home = ${PP_DIR}"
 
 UUID=$(/usr/libexec/plistbuddy -c Print:UUID /dev/stdin <<< `echo "${PROVISIONING_PROFILE}" | base64 -d | security cms -D`)
-
 echo "${PROVISIONING_PROFILE}" | base64 -d >${UUID}.mobileprovision
-
-#install profiles, will trigger xcode to install the profile
-open "${UUID}.mobileprovision"
-
-# wait for xcode to process the request
-sleep 10
-
-# shut down xcode (optional)
-kill -TERM $(ps aux | grep 'Xcode' | awk '{print $2}')
-
-ls -lah "${PP_DIR}"
-
-# UUID=$(/usr/libexec/plistbuddy -c Print:UUID /dev/stdin <<< `echo "${PROVISIONING_PROFILE}" | base64 -d | security cms -D`)
-# echo "${PROVISIONING_PROFILE}" | base64 -d >${UUID}.mobileprovision
-# md5 "${UUID}.mobileprovision"
-# mkdir -p "${PP_DIR}"
-# cp ${UUID}.mobileprovision "${PP_DIR}/"
+md5 "${UUID}.mobileprovision"
+mkdir -p "${PP_DIR}"
+cp ${UUID}.mobileprovision "${PP_DIR}/"
 
 echo ">> Build Provisioning Profile. 🤗"
 
